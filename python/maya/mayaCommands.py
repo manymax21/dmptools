80a799b88ab33023935a55fe2a158731d75f5698
Michael Havart <michael.havart@gmail.com>
2012-03-16 19:00:46 -0400

    update maya commands

--- python/maya/mayaCommands.py
+++ python/maya/mayaCommands.py
@@ -9,8 +9,52 @@ import maya.mel as mel
 import os
 import time
 import subprocess
+import fnmatch
 
 from dmptools.presets import PresetsManager
+
+def rotateScene():
+    global switchRotate
+    try:
+        switchRotate
+    except:
+        switchRotate = 1
+
+    if switchRotate == 1:
+        try:
+            cmds.setAttr('|Neo|generator_outpost_01_dm|Asset|Model_Orig_00|scene_grp.ry', 0.0)
+            switchRotate = 0
+        except:
+            pass
+        
+    elif switchRotate == 0:
+        try:
+            cmds.setAttr('|Neo|generator_outpost_01_dm|Asset|Model_Orig_00|scene_grp.ry', 45.0)
+            switchRotate = 1
+        except:
+            pass
+
+class HideShowNeoStuff(object):
+
+    def __init__(self):
+        self.stuffToHide = []
+        nodes = cmds.ls()
+        objFilter = ['Dest', 'Collision_hk', 'Orig_State', 'Dest_State']
+        
+        for node in nodes:
+            for f in objFilter:
+                if f in node:
+                    self.stuffToHide.append(node)
+                    break
+     
+    def hide(self):
+        for node in self.stuffToHide:
+            cmds.setAttr(node+'.visibility', False)
+    
+    def show(self):
+        for node in self.stuffToHide:
+            cmds.setAttr(node+'.visibility', True)
+     
 
 def deleteCollada():
     """ delete collada nodes"""
