9516287eced47caa27e129f7846dc3f92d71abf2
Michael Havart <michael.havart@gmail.com>
2012-03-20 19:12:01 -0400

    update install and neo

--- python/maya/neo.py
+++ python/maya/neo.py
@@ -7,27 +7,70 @@
 import maya.cmds as cmds
 import maya.mel as mel
 
+class States(object):
+    def __init__(self):
+        self.orig = ['Model_Orig_00']
+        self.dest = ['Model_Dest_01',
+                'Model_Dest_02',
+                'Model_Dest_01',
+                'Collision_hk_nav_model_Orig_00',
+                'Collision_hk_nav_model_Dest_01',
+                'Collision_hk_nav_model_Dest_02',
+                'Orig_State_00',
+                'Dest_State_01',
+                'Dest_State_02']
+        
+    def setOrig(self):
+        [cmds.setAttr(node+'.visibility', False) for node in self.dest]
+        [cmds.setAttr(node+'.visibility', True) for node in self.orig]
+
+    def setDest(self):
+        [cmds.setAttr(node+'.visibility', True) for node in self.dest]
+        [cmds.setAttr(node+'.visibility', False) for node in self.orig]
+
+def switchDestructionState():
+    states = States()
+    global switchDest
+    try:
+        switchDest
+    except:
+        switchDest = 1
+
+    if switchDest == 1:
+        try:
+            states.setOrig()
+            switchDest = 0
+        except:
+            pass
+        
+    elif switchDest == 0:
+        try:
+            states.setDest()
+            switchDest = 1
+        except:
+            pass
+            
 class HideShowNeoStuff(object):
 
     def __init__(self):
         self.stuffToHide = []
+        self.dest1Stuff = []
         nodes = cmds.ls()
         objFilter = ['Dest', 'Collision_hk', 'Orig_State', 'Dest_State']
-        
+
         for node in nodes:
             for f in objFilter:
                 if f in node:
                     self.stuffToHide.append(node)
                     break
-     
+
     def hide(self):
         for node in self.stuffToHide:
             cmds.setAttr(node+'.visibility', False)
     
     def show(self):
         for node in self.stuffToHide:
-            cmds.setAttr(node+'.visibility', True)
-     
+            cmds.setAttr(node+'.visibility', True)     
 def deleteCollada():
     """ delete collada nodes"""
     colladaNodes = []
